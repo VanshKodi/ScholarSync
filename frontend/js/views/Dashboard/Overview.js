30b412209b40a810522ceb38cf7829a7ab91ac52
Vansh Kodi <kodinariyavansh007@gmail.com>
2026-02-17 22:14:55 +0530

    Changed to cloudflare attempt 5

--- frontend/js/views/Dashboard/Overview.js
+++ frontend/js/views/Dashboard/Overview.js
@@ -1,6 +1,6 @@
 import { supabase } from "../../utils/supabase.js";
 
-const API_BASE = window.__ENV__.API_BASE_URL;
+const API_BASE = "https://api.vanshkodi.in";
 
 async function waitForUser(timeout = 5000) {
   const { data: sessionData } = await supabase.auth.getSession();
