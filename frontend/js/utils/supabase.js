add2eee7387c41c7f89e545eb12e42daac7c074e
Vansh Kodi <kodinariyavansh007@gmail.com>
2026-02-17 22:19:27 +0530

    Changed to cloudflare attempt 7

--- frontend/js/utils/supabase.js
+++ frontend/js/utils/supabase.js
@@ -1,6 +1,7 @@
 // js/scripts/supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-const { SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY } = ENV;
+
+const { SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY } = window.__ENV__;
 
 export const supabase = window.supabase.createClient(
   SUPABASE_PROJECT_URL,
