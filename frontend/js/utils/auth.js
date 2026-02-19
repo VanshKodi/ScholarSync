460cf82c0a0ec5e3ae29cebd758cc20ec9d28fba
Vansh Kodi <kodinariyavansh007@gmail.com>
2026-02-19 22:28:57 +0530

    Fix supabase import again

--- frontend/js/utils/auth.js
+++ frontend/js/utils/auth.js
@@ -8,7 +8,7 @@ export async function loginWithGoogle() {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/#/dashboard`
+      redirectTo: `${window.location.origin}`
     }
   });
   if (error) {
