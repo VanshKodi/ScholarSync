ab1e5e6ecb49394c4e052dfd1e9bf5f8ae3ccbb4
Vansh Kodi <kodinariyavansh007@gmail.com>
2026-02-19 14:52:22 +0530

    fixed supabase module error

--- frontend/js/utils/supabase.js
+++ frontend/js/utils/supabase.js
@@ -1,6 +1,3 @@
-// js/scripts/supabase.js
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
-
 const { SUPABASE_PROJECT_URL, SUPABASE_ANON_KEY } = window.__ENV__;
 
 export const supabase = window.supabase.createClient(
